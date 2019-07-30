20d8923c2434b7989c741e489644b195e3b1be3b
Kathy Warne <kathy.warne@intel.com>
2019-07-30 15:20:05 -0700

    using correct method

--- vars/woloxCi.groovy
+++ vars/woloxCi.groovy
@@ -21,7 +21,7 @@ def call(String yamlName) {
     def numsteps = projectConfig.steps.steps.size();
     println "Steps count= $numsteps";
 
-    def stepstr = projectConfig.steps.steps.toString();
+    def stepstr = projectConfig.steps.getString();
     println "Steps= $stepstr";
     println "Env= $projectConfig.environment";
     
