d74df4e1cd96c9e45d515e82f35a38976a6d7fec
Kathy Warne <kathy.warne@intel.com>
2019-07-31 14:36:29 -0700

    adding command exec

--- vars/buildSteps.groovy
+++ vars/buildSteps.groovy
@@ -3,14 +3,14 @@ import com.wolox.*;
 import com.wolox.steps.Step;
 
 def call(ProjectConfiguration projectConfig) {
-    println "Called buildsteps.groovy with args projectConfig: $projectConfig";
+    println "Called buildsteps.groovy"
     return { 
         List<Step> stepsA = projectConfig.steps.steps
         stepsA.each { step ->
             stage(step.name) {
                 step.commands.each { command ->
-                    //sh command
                     println "Command=$command"
+                    sh command
                     //sh label: 'Shell command execution', returnStdout: true, script: "$command"
                 }
             }
