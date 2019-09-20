582c981bb963d4f2bea9906fced648971eeddabd
Kathy Warne <kathy.warne@intel.com>
2019-09-20 13:45:41 -0700

    Moved workspace creation logic to yaml file

--- vars/woloxCi.groovy
+++ vars/woloxCi.groovy
@@ -5,7 +5,6 @@ import com.wolox.*;
 def call(String yamlName) {
     sh label: 'Shell command execution', returnStdout: true, script: 'echo Reading yaml file';
     sh label: 'Shell command execution', returnStdout: true, script: 'echo Current dir=`pwd`';
-    sh label: 'Shell command execution', returnStdout: true, script: 'echo Listing=\n';
     sh label: 'Shell command execution', returnStdout: true, script: "echo `ls -la`";
 
     def yaml = readYaml file: yamlName;
@@ -14,9 +13,52 @@ def call(String yamlName) {
     def buildNumber = Integer.parseInt(env.BUILD_ID)
     println "Build number= $buildNumber";
 
+    // create workspace
+
+    stage('CreateWS') {
+        sh label: 'Shell command execution', returnStdout: true, script: "echo `printenv | sort`";
+        def url = scm.getUserRemoteConfigs()[0].getUrl()
+        def repoName = url.tokenize('/').last().split("\\.git")[0] 
+        script {
+
+            gitVars = dir('ws/$repoName') {
+                git changelog: false,
+                credentialsId: 'kmw-github-cred',
+                poll: false,
+                url: '$url',
+            }
+
+            env.GIT_COMMIT = gitVars.GIT_COMMIT
+            env.GIT_LOCAL_BRANCH = gitVars.GIT_LOCAL_BRANCH
+            env.GIT_BRANCH = gitVars.GIT_BRANCH
+            println "Branch=$env.GIT_LOCAL_BRANCH"
+            println "Branch is $env.BRANCH_NAME"
+            println "Local branch is $env.GIT_LOCAL_BRANCH"
+
+            sh label: 'Shell command execution', returnStdout: true,
+               script: "printenv | sort";
+
+        }
+
+        env.WSDIR=env.WORKSPACE + '/ws'
+        println "Workdir=$env.WSDIR"
+
+        if ( env.CHANGE_BRANCH ) {
+            env.LOCAL_BRANCH=env.CHANGE_BRANCH
+        } else if ( env.BRANCH_NAME ) {
+            env.LOCAL_BRANCH=env.BRANCH_NAME
+        } else {
+            env.LOCAL_BRANCH=env.GIT_BRANCH.drop(env.GIT_BRANCH.indexOf('/')+1)
+        }
+
+        sh label: 'Shell command execution', returnStdout: true,
+           script: "cd $WSDIR/$repoName && git checkout $LOCAL_BRANCH";
+
+        buildDescription 'Workspace checkout is complete.'
+    }
+
     // load project's configuration
     ProjectConfiguration projectConfig = ConfigParser.parse(yaml, env);
-    println "Services= $projectConfig.services";
 
     def numsteps = projectConfig.steps.steps.size();
     println "Steps count= $numsteps";
