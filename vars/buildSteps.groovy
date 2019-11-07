305b1505884d322fd04ab367ed69c4a553794bce
Kathy Warne <kathy.warne@intel.com>
2019-11-06 21:56:22 -0800

    Adding task name to stage name display

--- vars/buildSteps.groovy
+++ vars/buildSteps.groovy
@@ -8,7 +8,7 @@ def call(ProjectConfiguration projectConfig) {
       task.osMatrix.each { k,v ->
         node("${k}") {
           println "workspace type: $task.wsType"
-          stage("create workspace-$k") {
+          stage("$task.name-create workspace-$k") {
             deleteDir()
             def wscreate = "$task.wsType"(projectConfig.environment,
                                           projectConfig.timeout)
@@ -17,7 +17,7 @@ def call(ProjectConfiguration projectConfig) {
 
           List<Step> stepsA = task.steps.steps
           stepsA.each { step ->
-            stage("$step.name-$k") {
+            stage("$task.name-$step.name-$k") {
               timeout(time: projectConfig.timeout) {
                 withEnv(projectConfig.environment) {
                   def closure = "${v}"(step)
