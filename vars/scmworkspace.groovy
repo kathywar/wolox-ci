89039ce28a745d82de108f691a8577ab74d4dd1a
Kathy Warne <kathy.warne@intel.com>
2020-01-02 11:25:53 -0800

    To determine branch for initial checkout, use DEFAULT_BRANCH env var as a last resort

--- vars/scmworkspace.groovy
+++ vars/scmworkspace.groovy
@@ -1,27 +1,15 @@
-import com.wolox.*;
+import com.wolox.*
 
 def call( def projenv, def maxtime) {
   return {
     timeout(time: maxtime) {
       withEnv(projenv) {
-        def url = scm.getUserRemoteConfigs()[0].getUrl()
-        def repoName = url.tokenize('/').last().split("\\.git")[0]
-        echo sh(returnStdout: true, script: 'printenv | sort')
-        script {
-
-          println "Credential: $env.CREDENTIAL" 
-          gitVars = dir("ws/$repoName") {
-            git changelog: false,
-            credentialsId: env.CREDENTIAL,
-            poll: false,
-            url: "$url"
-          }
  
          if ( env.CHANGE_BRANCH ) {
-            env.LOCAL_BRANCH=env.CHANGE_BRANCH
+            env.LOCAL_BRANCH=env.CHANGE_BRANCH  //build triggered by multibranch job
           } else if ( env.BRANCH_NAME ) {
             env.LOCAL_BRANCH=env.BRANCH_NAME
-          } else if ( env.GERRIT_BRANCH ) {
+          } else if ( env.GERRIT_BRANCH ) { // build triggered by gerrit change
             env.LOCAL_BRANCH=env.GERRIT_BRANCH
             if ( GERRIT_EVENT_TYPE=='change-merged') {
                 env.TREEISH=env.GERRIT_NEWREV
@@ -35,15 +23,32 @@ def call( def projenv, def maxtime) {
               env.TREEISH=env.ghprbActualCommit
           } else if ( env.GIT_BRANCH ) {
               def str=env.GIT_BRANCH
-              env.LOCAL_BRANCH=str.substring(str.lastIndexOf("origin/") + 1, str.length())
+              env.LOCAL_BRANCH=str.substring(str.lastIndexOf("origin/") + 7, str.length())
+          } else {
+              println "Jenkins did not set a branch name, using DEFAULT_BRANCH set in Jenkinsfile"
+              env.LOCAL_BRANCH=env.DEFAULT_BRANCH
+          }
+
+          def url = scm.getUserRemoteConfigs()[0].getUrl()
+          def repoName = url.tokenize('/').last().split("\\.git")[0]
+          echo sh(returnStdout: true, script: 'printenv | sort')
+          script {
+
+            println "Credential: $env.CREDENTIAL"
+            gitVars = dir("ws/$repoName") {
+              git changelog: false,
+              credentialsId: env.CREDENTIAL,
+              poll: false,
+              url: "$url",
+              branch: "$env.LOCAL_BRANCH"
           }
 
           if ( env.CHANGE_SPEC ) {
               sh 'pwd'
               sh "cd ws/$repoName && git fetch origin $env.CHANGE_SPEC:jenkins-branch"
-          } else {
-              sh "cd ws/$repoName && git checkout $env.LOCAL_BRANCH"
-          }
+          } // else {
+       //       sh "cd ws/$repoName && git checkout $env.LOCAL_BRANCH"
+       //   }
 
           env.GIT_COMMIT = gitVars.GIT_COMMIT
           env.GIT_LOCAL_BRANCH = gitVars.GIT_LOCAL_BRANCH
