f040050605e4d5a933237a01f3b2232b1f30a82b
Kathy Warne <kathy.warne@intel.com>
2020-01-02 09:35:09 -0800

    Simplify script call on windows

--- vars/windows.groovy
+++ vars/windows.groovy
@@ -4,19 +4,7 @@ import com.wolox.steps.Step;
 
 def call(Step step) {
   return {
-
-    env.WSTOP=env.WORKSPACE.replaceAll('\\\\','/')
-
-    def echoscript = "#!/bin/bash\nset -eE -o pipefail\n"
-    step.commands.each { 
-        echoscript = echoscript + "echo + \"${it}\"\n" + "${it}\n" 
-    }
-    writeFile file: "$WORKSPACE\\icl-pipeline.sh", text: """cd $WSTOP\n$echoscript"""
-    env.FILEPATH="$env.WSTOP/icl-pipeline.sh"
-    echo bat (returnStdout:true,
-              script: """call r:\\u4win\\u4w_ksh.bat /c %FILEPATH%
-                         exit %ERRORLEVEL
-                      """)
-    bat returnStdout: true, script: 'del %WORKSPACE%\\icl-pipeline.sh'
+    def scr = step.script()
+    echo bat( returnStdout: true, script: """ sh -x -c \"${scr}\" """).trim()
   }
 }
