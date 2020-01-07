eaccd5ead64351a809cde269dd1873a55cbbd111
Kathy Warne <kathy.warne@intel.com>
2020-01-06 17:58:47 -0800

    Fixing refspecs and branch names

--- vars/scmworkspace.groovy
+++ vars/scmworkspace.groovy
@@ -1,5 +1,3 @@
-import com.wolox.*
-
 def call( def os, def projenv, def maxtime) {
   return {
     timeout(time: maxtime) {
@@ -8,6 +6,8 @@ def call( def os, def projenv, def maxtime) {
         list0.add("printenv | sort")
         def closure ="$os"(list0)
         closure()
+
+        env.REFSPEC="+refs/heads/*:refs/remotes/*"
 
         if ( env.CHANGE_BRANCH ) {
           env.LOCAL_BRANCH=env.CHANGE_BRANCH  //build triggered by multibranch job
@@ -18,13 +18,12 @@ def call( def os, def projenv, def maxtime) {
           if ( GERRIT_EVENT_TYPE=='change-merged') {
             env.LOCAL_BRANCH=env.GERRIT_NEWREV
           } else {
+            env.REFSPEC="$env.GERRIT_REFSPEC:$env.GERRIT_REFSPEC"
             env.LOCAL_BRANCH=env.GERRIT_REFSPEC
-            env.TREEISH=env.GERRIT_BRANCH
           }
         } else if ( env.ghprbPullId ) {  // github pull request builder
-          //env.LOCAL_BRANCH=env.ghprbTargetBranch
-          env.LOCAL_BRANCH=env.ghprbPullId
-          env.TREEISH=env.ghprbActualCommit
+          env.LOCAL_BRANCH="pull/$env.ghprbPullId/head"
+          env.REFSPEC="refs/pull/$env.ghprbPullId/head:refs/pull/$env.ghprbPullId/head"
         } else if ( env.GIT_BRANCH ) {
           def str=env.GIT_BRANCH
           env.LOCAL_BRANCH=str.substring(str.lastIndexOf("origin/") + 7, str.length())
@@ -35,28 +34,28 @@ def call( def os, def projenv, def maxtime) {
 
         def url = scm.getUserRemoteConfigs()[0].getUrl()
         def repoName = url.tokenize('/').last().split("\\.git")[0]
+
+        println "LOCAL BRANCH: " + env.LOCAL_BRANCH
+        println "REFSPEC: " + env.REFSPEC
+
         script {
-
           println "Credential: $env.CREDENTIAL"
-          gitVars = dir("ws/$repoName") {
-            git changelog: false,
-            credentialsId: env.CREDENTIAL,
-            poll: false,
-            url: "$url",
-            branch: "$env.LOCAL_BRANCH"
-          }
-
-          if ( env.CHANGE_SPEC ) {
-            println "Checking out remote changeset patch"
-          }
-
-          env.GIT_COMMIT = gitVars.GIT_COMMIT
-          env.GIT_LOCAL_BRANCH = gitVars.GIT_LOCAL_BRANCH
-          env.GIT_BRANCH = gitVars.GIT_BRANCH
-
-          println "Local branch is $env.GIT_LOCAL_BRANCH"
-
+          gitVars = checkout([$class: 'GitSCM', 
+            branches: [[name: "$LOCAL_BRANCH"]], 
+            doGenerateSubmoduleConfigurations: false, 
+            extensions: [[$class: 'RelativeTargetDirectory', 
+            relativeTargetDir: 'ws'], [$class: 'WipeWorkspace']], 
+            submoduleCfg: [], 
+            userRemoteConfigs: [[credentialsId: env.CREDENTIAL, 
+            refspec: "$REFSPEC", 
+            url: url ]]])
         }
+
+        env.GIT_COMMIT = gitVars.GIT_COMMIT
+        env.GIT_LOCAL_BRANCH = gitVars.GIT_LOCAL_BRANCH
+        env.GIT_BRANCH = gitVars.GIT_BRANCH
+
+        println "Local branch is $env.GIT_LOCAL_BRANCH"
 
         env.WSDIR=env.WORKSPACE + '/ws'
         env.REPO_PATH=env.WSDIR + "/$repoName"
